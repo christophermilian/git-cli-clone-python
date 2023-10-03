25f00ba08173bc4883da9858be3ec6edb6ee4e08
Christopher Milian <christophermilian16@gmail.com>
2023-10-03 14:30:25 -0500

    Docs: Update README, Struct: Setup files

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # git-cli-clone-python
 A Python based Git clone that allows creating repos, commiting, and pushing.
+
+## Installing packages
+There is an included `requirements.txt` file containing the required modules for this project.
+
+To install, use the following command:
+```
+$ python3 -m pip install -r requirements.txt
+```
+You should now have the following installed:
+- virtualenv
+- wheel
+- setuptools
+- twine
+- click>=8.1.7
+
+## Python Version
+Python 3.11.5
---
+++ setup.py
@@ -0,0 +1,30 @@
+"""Module to help in tool packaging"""
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read()
+    setup(
+    name = 'Python Git Clone',
+    version = '0.0.1',
+    author = 'Christopher Milian',
+    author_email = 'christophermilian16@gmail.com',
+    license = 'MIT License',
+    description = 'Python based Git clone allowing one to create repos, commit, and push.',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = 'https://github.com/christophermilian/git-cli-clone-python',
+    py_modules = ['git_clone', 'app'],
+    packages = find_packages(),
+    install_requires = [requirements],
+    python_requires='>=3.11',
+    classifiers=[
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent",
+    ],
+    entry_points = '''
+        [console_scripts]
+        pygit=git_clone:cli
+    '''
+)
